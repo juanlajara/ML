2ac5176145c8df875c7c82a586b5672cc82e4582
juanlajara <juanlajara001@gmail.com>
2023-05-04 22:35:26 -0500

    create chunk splitter and text to page

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 
 def main():
     load_dotenv()
-    print(os.getenv("OPENAI_API_KEY"))
+    # print(os.getenv("OPENAI_API_KEY"))
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF")
+
+    # upload pdf
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # extract text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separators="/n",
+            chunk_size=1000,
+            chunk_overlap=500,
+            length_function=len
+        )
+
+        st.write(text)
 
 
 if __name__ == "__main__":
